a4cfb0f798530e34bd38413840a6ebd9dd0debf5
jimmyberg <jimmyberg.kim@gmail.com>
2025-04-16 20:29:21 +0900

    chore: ktca module 의존성 정리

--- ktca/build.gradle.kts
+++ ktca/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     // Module-specific dependencies
     implementation(project(":common"))
 
-    // Redis dependency (specific to ktc module)
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
