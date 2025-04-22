c3c4227dbc31d6619d3d8f17d6065956be515ff1
jimmyberg <jimmyberg.kim@gmail.com>
2025-04-22 14:18:29 +0900

    feat: ktc module redis 관련 의존성 변경

--- ktc/build.gradle.kts
+++ ktc/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
 
     kapt("org.springframework.boot:spring-boot-configuration-processor")
