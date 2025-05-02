3da89b736bce89f35d44cf05f25c7d90b159dd86
jimmyberg <jimmyberg.kim@gmail.com>
2025-05-02 12:24:59 +0900

    feat: v1 트래픽 현황 message publish 관련 공통 기능 개발 추가

---
+++ common/src/main/kotlin/com/kona/common/infrastructure/message/rabbitmq/dto/V1TrafficStatusMessage.kt
@@ -0,0 +1,12 @@
+package com.kona.common.infrastructure.message.rabbitmq.dto
+
+import com.kona.common.infrastructure.message.rabbitmq.BaseMessage
+
+data class V1TrafficStatusMessage(
+    val zoneId: String,
+    val token: String,
+    val clientIp: String?,
+    val clientAgent: String?,
+    val waitingNumber: Long,
+    val estimatedTime: Long
+) : BaseMessage()
--- common/src/test/kotlin/com/kona/common/infrastructure/message/rabbitmq/MessagePublisherImplTest.kt
+++ common/src/test/kotlin/com/kona/common/infrastructure/message/rabbitmq/MessagePublisherImplTest.kt
@@ -1,5 +1,6 @@
 package com.kona.common.infrastructure.message.rabbitmq
 
+import com.kona.common.infrastructure.message.rabbitmq.dto.V1TrafficStatusMessage
 import com.kona.common.infrastructure.util.CORRELATION_ID_HEADER_FIELD
 import com.kona.common.testsupport.rabbit.MockRabbitMQ
 import com.kona.common.testsupport.rabbit.MockRabbitMQTestListener
@@ -14,12 +15,17 @@ class MessagePublisherImplTest : StringSpec({
     val mockRabbitTemplate = MockRabbitMQ.rabbitTemplate
     val messagePublisher = MessagePublisherImpl(mockRabbitTemplate)
 
-    data class TestMessage(val message: String) : BaseMessage()
-
-    "RabbitMQ Test Message 발행하여 결과 정상 확인한다" {
+    "V1TrafficStatusMessage 발행 결과 정상 확인한다" {
         // given
         val exchange = MessageExchange.V1_SAVE_TRAFFIC_STATUS_EXCHANGE
-        val message = TestMessage("Hello World!!")
+        val message = V1TrafficStatusMessage(
+            zoneId = "TEST_ZONE",
+            token = "token-1",
+            clientIp = "127.0.0.1",
+            clientAgent = "ANDROID",
+            waitingNumber = 1,
+            estimatedTime = 60000
+        )
 
         // when
         messagePublisher.publishDirectMessage(exchange, message)
@@ -28,8 +34,13 @@ class MessagePublisherImplTest : StringSpec({
         val received = mockRabbitTemplate.receive(MessageQueue.V1_SAVE_TRAFFIC_STATUS_QUEUE)!!
         received.messageProperties.headers[CORRELATION_ID_HEADER_FIELD] shouldNotBe null
 
-        val result = mockRabbitTemplate.messageConverter.fromMessage(received) as TestMessage
-        result.message shouldBe "Hello World!!"
+        val result = mockRabbitTemplate.messageConverter.fromMessage(received) as V1TrafficStatusMessage
+        result.zoneId shouldBe "TEST_ZONE"
+        result.token shouldBe "token-1"
+        result.clientIp shouldBe "127.0.0.1"
+        result.clientAgent shouldBe "ANDROID"
+        result.waitingNumber shouldBe 1
+        result.estimatedTime shouldBe 60000
     }
 
 })
